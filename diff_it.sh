dbf3ed6d550ea76d1ce402e9131585ba9ee53c74
slutymeme <maccary.noel@gmail.com>
2020-05-30 04:13:09 +0200

    fixed diff script

--- diff_it.sh
+++ diff_it.sh
@@ -2,5 +2,5 @@
 mkdir -p bin diffs
 clang -w -fsanitize=address -o bin/test_diff.out src/test_diff.c ../libftprintf.a \
 && ./bin/test_diff.out > diffs/ft_printf.diff \
-&& ./bin/test_diff.out > diffs/printf.diff \
+&& ./bin/test_diff.out printf > diffs/printf.diff \
 && diff diffs/ft_printf.diff diffs/printf.diff
