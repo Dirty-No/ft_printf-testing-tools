c2745a0eb429d82ece891dc77feb4e9db6e26715
Anne LEZAN <alezan@e2r2p3.42.fr>
2020-08-03 14:16:15 +0200

    fixed ouput

--- diff_it.sh
+++ diff_it.sh
@@ -5,6 +5,6 @@ mkdir -p bin outputs
 clang -w -g -fsanitize=address -o bin/test_diff.out src/tests.c src/test_diff.c ../libftprintf.a 
 ./bin/test_diff.out > outputs/ft_printf.output 
 ./bin/test_diff.out printf > outputs/printf.output 
-echo -e "\e[31m" 
+printf "\e[31m" 
 diff outputs/ft_printf.output outputs/printf.output 
-echo -e "\e[0m"JJ
+printf "\e[0m"JJ
